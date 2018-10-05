a3db08b7a62e4e539af04bb79aae84afe5540e92
jesusmlg <jesusmlg@gmail.com>
2018-10-05 11:27:06 +0200

    begging

---
+++ database/migrations/2018_10_04_154937_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',255);
+            $table->text('description');
+            $table->integer('user_id');
+            $table->enum('category',['work','home','hobby']);
+            $table->integer('state',['pending','done','canceled']);
+            $table->integer('priority',['log','medium','high']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
